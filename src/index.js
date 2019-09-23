56b4bdb1d616c4521ade1d88778b9204fad14ad2
saja amer <sajaamer86@gmail.com>
2019-09-23 05:00:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let taskForm = document.getElementById("create-task-form");
   
   taskForm.addEventListener("submit",(e)=>{
-    e.preventDefault();let newTaskInput=document.getElementById("new-task-description").value;
+    e.preventDefault();
+    let newTaskInput=document.getElementById("new-task-description").value;
   let li = document.createElement("li");
   document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput ;
   });
