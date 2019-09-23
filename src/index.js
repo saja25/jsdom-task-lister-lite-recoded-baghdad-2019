810e1f7e6a246f82c75e3546cf3a36964b5fec5b
saja amer <sajaamer86@gmail.com>
2019-09-23 05:02:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let taskForm = document.getElementById("create-task-form");
+  
+  taskForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let newTaskInput=document.getElementById("new-task-description").value;
+  let li = document.createElement("li");
+  document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput ;
+  });
+ 
+})
