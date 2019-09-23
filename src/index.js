6bd33e407ebf727aea5f4e9ac913c09aada00b0f
saja amer <sajaamer86@gmail.com>
2019-09-23 05:00:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   taskForm.addEventListener("submit",(e)=>{
     e.preventDefault();
   let li = document.createElement("li");
-  let button=document.createElement("button");
   document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput ;
-  document.getElementById("tasks").appendChild(button).innerHTML="X";
   });
  
 })
