96e598651125cd4ffd3a0e04da730e66b5dab524
saja amer <sajaamer86@gmail.com>
2019-09-23 05:00:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,19 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
   let li = document.createElement("li");
   let button=document.createElement("button");
-  document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput
+  document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput ;
   document.getElementById("tasks").appendChild(button).innerHTML="X";
   });
-  newTaskInput.addEventListener("click", (e) => {
-        e.preventDefault();
-    if (e.target.nodeName === "BUTTON") {
-     li.remove();
-     button.remove();
-      
- //   taskList.deleteTask(e.target.dataset.description);
- //   renderApp();
-    }
-  });
  
-
 })
