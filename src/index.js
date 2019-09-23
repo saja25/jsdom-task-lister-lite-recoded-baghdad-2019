c04f7e7eb4e9ef94bd7f8b0b2e5d75c6d5f3f24b
saja amer <sajaamer86@gmail.com>
2019-09-23 04:44:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+  let li = document.createElement("li");
+  let newTaskInput=document.getElementById("new-task-description").value;
+  document.getElementById("tasks").appendChild(li).innerHTML=newTaskInput;
+  let button=document.createElement("button");
+  document.getElementById("tasks").appendChild(button).innerHTML="X";
+  });
+  newTaskInput.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      e.preventDefault();
+     li.remove();
+     button.remove();
+      
+ //   taskList.deleteTask(e.target.dataset.description);
+ //   renderApp();
+    }
+  });
+ 
+
+})
